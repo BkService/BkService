85df3e5a66182c6d1deb609f2eb3b6e2ca3b8512
Kovalev Vadim <vd.kv@ya.ru>
2013-10-31 02:33:55 +0400

    add interface for EventManager and its implemetation, Market and MarketResult

---
+++ BKService/src/junior/server/core/data/events/EventManager.java
@@ -0,0 +1,28 @@
+package junior.server.core.data.events;
+
+import java.util.HashMap;
+
+/**
+ * Реализация интерфейса EventManagerInterface
+ * @author kovalev
+ *
+ */
+public class EventManager implements EventManagerInterface {
+	HashMap<Integer, Event> eventsMap;	
+	
+	@Override
+	public Event createEvent(Event new_event) {
+		return eventsMap.put(new_event.getEventId(), new_event);
+	}
+
+	@Override
+	public Event getEvent(int event_id) {
+		return eventsMap.get(event_id);
+	}
+
+	@Override
+	public Event removeEvent(int event_id) {
+		return eventsMap.remove(event_id);
+	}
+	
+}

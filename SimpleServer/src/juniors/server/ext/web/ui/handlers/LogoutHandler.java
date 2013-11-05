97c2be927a3d6288600d2d48728397735503e01c
watson <watson94@gmail.com>
2013-11-06 02:55:00 +0400

    Removed annotation.

--- SimpleServer/src/juniors/server/ext/web/ui/handlers/LogoutHandler.java
+++ SimpleServer/src/juniors/server/ext/web/ui/handlers/LogoutHandler.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/LogoutHandler")
-
 public class LogoutHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  

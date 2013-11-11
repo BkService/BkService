a80284647222a1532031ead334bc4730ffcaf8e8
watson <watson94@gmail.com>
2013-11-11 05:09:59 +0400

    Removed deprecated calls. And added stub to ResultProvider.

---
+++ SimpleServer/src/juniot/server/core/resultprovider/ResultProvider.java
@@ -0,0 +1,39 @@
+package juniot.server.core.resultprovider;
+
+import java.util.concurrent.TimeUnit;
+
+import juniors.server.core.logic.RunnableService;
+
+public class ResultProvider implements RunnableService {
+
+    @Override
+    public void start() {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void stop() {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public boolean isStarted() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public long getDelay() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public TimeUnit getTimeUnitDelay() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
